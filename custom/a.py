12b8329a162c47f7ef553fe564547f206686c85b
vyaskartik20 <vyas.2@iitj.ac.in>
2021-02-23 09:34:47 +0530

    audio input to text added

--- custom/a.py
+++ custom/a.py
@@ -14,7 +14,23 @@ def speak(audio) :
     print(audio)
     engine.runAndWait()
     
-
+def takeCommand():
+    r =sr.Recognizer()
+    with sr.Microphone() as source :
+        print("Listening ...")
+        r.pause_threshold = 1
+        audio = r.listen(source, timeout = 3, phrase_time_limit = 5 )
+        
+    try : 
+        print("Recognizing ...")
+        query = r.recognize_google(audio, language = "en-in")
+        print(f"You said : {query}")
+        
+    except :
+        speak("Say that again please ...")
+        return "none"
+    
+    return query
 
 if __name__ == "__main__" : 
     # speak(" input in here")
